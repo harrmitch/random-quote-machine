774faacae4105979f227d53514a125f613a10f5c
harrmitch <minhyh0987@gmail.com>
2022-07-07 11:08:15 +0700

    add header and footer

--- src/App.svelte
+++ src/App.svelte
@@ -33,6 +33,23 @@
   $: document.documentElement.style.setProperty("--color", color);
 </script>
 
+<header>
+  <nav class="navbar bg-light">
+    <div class="container-fluid">
+      <span class="navbar-brand fw-bold">
+        <img
+          src="src/assets/favicon.svg"
+          alt="Logo"
+          width="30"
+          height="24"
+          class="d-inline-block align-text-top"
+        />
+        Random Quote Machine
+      </span>
+    </div>
+  </nav>
+</header>
+
 <main>
   {#if currentQuote}
     <div
@@ -82,6 +99,13 @@
     </div>
   {/if}
 </main>
+
+<footer class="text-white text-center mt-2">
+  By <a
+    class="text-white text-decoration-none fw-bold"
+    href="https://github.com/harrmitch">harrmitch</a
+  >
+</footer>
 
 <style>
   :global(body) {
