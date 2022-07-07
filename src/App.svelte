a4dae2679c9448c717ce1d988986cbfe703eedaf
harrmitch <minhyh0987@gmail.com>
2022-07-07 10:44:17 +0700

    add transition effects

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,6 @@
 <script>
   import { onMount } from "svelte";
+  import { fade } from "svelte/transition";
   import randomColor from "randomcolor";
 
   const getQuote = async () => {
@@ -34,38 +35,50 @@
 
 <main>
   {#if currentQuote}
-    <div class="container bg-white p-5 mt-5 rounded-2" id="quote-box">
-      <section>
-        <figure>
-          <blockquote class="text-center">
-            <p style:color id="text" class="fs-3">
-              <i class="bi bi-quote" />
-              {currentQuote.content}
-            </p>
-          </blockquote>
-          <figcaption style:color class="fs-5 text-end fst-italic" id="author">
-            - {currentQuote.author}
-          </figcaption>
-        </figure>
-      </section>
+    <div
+      transition:fade
+      class="container bg-white p-5 mt-5 rounded-2"
+      id="quote-box"
+    >
+      {#key currentQuote}
+        <section in:fade>
+          <figure>
+            <blockquote class="text-center">
+              <p style:color id="text" class="fs-3">
+                <i class="bi bi-quote" />
+                {currentQuote.content}
+              </p>
+            </blockquote>
+            <figcaption
+              style:color
+              class="fs-5 text-end fst-italic"
+              id="author"
+            >
+              - {currentQuote.author}
+            </figcaption>
+          </figure>
+        </section>
+      {/key}
 
-      <section>
-        <div class="d-inline pt-3">
-          <a
-            style:background-color={color}
-            class="btn text-white"
-            id="tweet-quote"
-            href={`https://twitter.com/intent/tweet?hashtags=quotes&text="${currentQuote.content}" ${currentQuote.author}`}
-            target="_blank"><i class="bi bi-twitter" /></a
-          >
-          <a
-            style:background-color={color}
-            class="btn text-white float-end"
-            id="new-quote"
-            on:click={getNewQuote}>New quote</a
-          >
-        </div>
-      </section>
+      {#key color}
+        <section>
+          <div in:fade class="d-inline pt-3">
+            <a
+              style:background-color={color}
+              class="btn text-white"
+              id="tweet-quote"
+              href={`https://twitter.com/intent/tweet?hashtags=quotes&text="${currentQuote.content}" ${currentQuote.author}`}
+              target="_blank"><i class="bi bi-twitter" /></a
+            >
+            <a
+              style:background-color={color}
+              class="btn text-white float-end"
+              id="new-quote"
+              on:click={getNewQuote}>New quote</a
+            >
+          </div>
+        </section>
+      {/key}
     </div>
   {/if}
 </main>
